02fd6458874636bce790948a8d6aef72256499a6
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-08-25 17:26:06 +0200

    Minor update

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
+const path = require('path');
 const verifyImage = require('./checkImage');
 const convertImage = require('./convertImage');
+const { DEFAULT_IMAGE, DEFAULT_OUTPUT } = require('./utils/constants');
 
 (async () => {
     try {
-        // Replace with your image path
-        const filePath = 'path_to_image.png'; // Replace with your image path
+        const defaultPath = path.join(__dirname, DEFAULT_IMAGE);
+        const imagePath = process.argv[2] || defaultPath;
 
-        // Check the image
-        const isValid = await verifyImage(filePath);
-        if (isValid) {
-            console.log("Image is valid!");
-        }
+        // Verify the image
+        const valid = await verifyImage(imagePath);
+        console.log(valid ? `✅ Verified badge pic ('${imagePath}') successfully!` : `❌ '${imagePath}' is not a valid badge image!`);
 
-        // Convert the image
-        await convertImage(filePath);
-        console.log("Image converted successfully!");
+        // Convert the image (can accept any valid image format)
+        const outputFilename = process.argv[3] || DEFAULT_OUTPUT;
+        await convertImage(imagePath, outputFilename)
+        .then(() => console.log(`✅ Converted '${imagePath}' to '${outputFilename}' successfully!`))
+        .error(err => console.error(err.message));
+        
     } catch (error) {
         console.error(error.message);
     }
-
 })();
