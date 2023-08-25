86c6a248e740a61de0b6ed4f97daa5149e6b53ec
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-08-25 17:23:21 +0200

    Updated convertImage

--- convertImage.js
+++ convertImage.js
@@ -1,21 +1,40 @@
 const sharp = require('sharp');
+const { MAX_WIDTH, MAX_HEIGHT, DEFAULT_OUTPUT } = require('./utils/constants');
 
-function convertImage(filePath) {
-    const center = { x: 256, y: 256 };
+/**
+ * Converts the image to the required badge format
+ * @param {string} filePath - Path to the image file
+ * @param {string} outputPath - Path to the output file
+ * @returns {Promise} - Promise that resolves when the image is converted
+ * @throws {Error} - If the image cannot be converted
+ * @example
+ * 
+ * convertImage('image.png', 'output.png');
+ * // => Promise
+ * 
+ * convertImage('image.png', 'output.png')
+ *    .then(() => console.log('Image converted!'))
+ *   .catch(err => console.error(err.message));
+ * // => Image converted!
+ *  
+ **/
+function convertImage(filePath, outputPath = DEFAULT_OUTPUT) {
+    const center = { x: MAX_WIDTH / 2, y: MAX_HEIGHT / 2 }; // Center of the circle
 
+    // Resize the image to 512x512 and convert to sRGB
     return sharp(filePath)
-        .resize(512, 512)
+        .resize(MAX_WIDTH, MAX_HEIGHT)
         .toColourspace('srgb')
         .raw()
         .toBuffer({ resolveWithObject: true })
         .then(({ data, info }) => {
+            // Make pixels outside circle transparent
             for (let y = 0; y < info.height; y++) {
                 for (let x = 0; x < info.width; x++) {
                     const idx = (y * info.width + x) * info.channels;
                     const dist = Math.sqrt(Math.pow(center.x - x, 2) + Math.pow(center.y - y, 2));
-
                     if (dist > 256) {
-                        data[idx + 3] = 0; // set alpha to 0 for pixels outside circle
+                        data[idx + 3] = 0; 
                     }
                 }
             }
@@ -25,7 +44,10 @@ function convertImage(filePath) {
                     height: info.height,
                     channels: info.channels
                 }
-            }).png().toFile('convertedImage.png');
+            }).png().toFile(outputPath);
+        })
+        .catch(err => {
+            throw new Error(`Error converting image: ${err.message}`);
         });
 }
 
