b53559982d40f60109dcb9ef50a7f4c8f82c27c8
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-08-25 18:36:21 +0200

    Minor update

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const path = require('path');
 const verifyImage = require('./checkImage');
 const convertImage = require('./convertImage');
@@ -5,19 +6,40 @@ const { DEFAULT_IMAGE, DEFAULT_OUTPUT } = require('./utils/constants');
 
 (async () => {
     try {
-        const defaultPath = path.join(__dirname, DEFAULT_IMAGE);
-        const imagePath = process.argv[2] || defaultPath;
+        const command = process.argv[2];
 
-        // Verify the image
-        const valid = await verifyImage(imagePath);
-        console.log(valid ? `✅ Verified badge pic ('${imagePath}') successfully!` : `❌ '${imagePath}' is not a valid badge image!`);
+        switch (command) {
+            case 'verify':
+                const verifyImagePath = process.argv[3] || path.join(__dirname, DEFAULT_IMAGE);
 
-        // Convert the image (can accept any valid image format)
-        const outputFilename = process.argv[3] || DEFAULT_OUTPUT;
-        await convertImage(imagePath, outputFilename)
-        .then(() => console.log(`✅ Converted '${imagePath}' to '${outputFilename}' successfully!`))
-        .error(err => console.error(err.message));
-        
+                // Check if file exists
+                if (!fs.existsSync(verifyImagePath)) {
+                    throw new Error(`❌ File '${verifyImagePath}' does not exist.`);
+                }
+
+                // Verify the image
+                const valid = await verifyImage(verifyImagePath);
+                console.log(valid ? `✅ Verified badge pic ('${verifyImagePath}') successfully!` : `❌ '${verifyImagePath}' is not a valid badge image!`);
+                break;
+
+            case 'convert':
+                const convertImagePath = process.argv[3] || path.join(__dirname, DEFAULT_IMAGE);
+                const outputFilename = process.argv[4] || DEFAULT_OUTPUT;
+
+                // Check if file exists
+                if (!fs.existsSync(convertImagePath)) {
+                    throw new Error(`❌ File '${convertImagePath}' does not exist.`);
+                }
+
+                // Convert the image (can accept any valid image format)
+                await convertImage(convertImagePath, outputFilename)
+                    .then(() => console.log(`✅ Converted '${convertImagePath}' to '${outputFilename}' successfully!`))
+                    .catch(err => console.error(err.message));
+                break;
+
+            default:
+                console.error("❌ Invalid command. Use 'verify' or 'convert'.");
+        }
     } catch (error) {
         console.error(error.message);
     }
